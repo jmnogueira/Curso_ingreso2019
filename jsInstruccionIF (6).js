7aa28c422ee7eb1c50041242d8771445842c26ff
jmnogueira <52511619+jmnogueira@users.noreply.github.com>
2019-07-13 07:07:23 -0300

    Add files via upload

---
+++ jsInstruccionIF (6).js
@@ -0,0 +1,33 @@
+function mostrar()
+{
+//tomo la edad 
+var edad;
+edad=document.getElementById("edad").value;
+if (edad<13)
+{
+    alert("es menor");
+}
+    else if(edad>13 && edad<=17)
+    {
+        alert("es adolescente");
+    }
+    else 
+    {
+        alert("es mayor");
+    }
+
+
+        
+    
+           
+
+    
+
+
+    
+
+
+
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,18 @@
+function mostrar()
+{
+//tomo la edad  
+var edad;
+var estadoCivil;
+
+edad=document.getElementById("edad").value;
+estadoCivil=document.getElementById("estadoCivil").value;
+
+if (edad<17 && estadoCivil!="Soltero")
+{
+    alert("es muy pequeño para no ser soltero");
+}
+
+	
+
+
+}//FIN DE LA FUNCIÓN
