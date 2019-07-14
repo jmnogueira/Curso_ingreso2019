44c457696046df0cab6e8d3607badf8e7c535505
jmnogueira <52511619+jmnogueira@users.noreply.github.com>
2019-07-13 21:16:03 -0300

    Add files via upload

--- cuatro.js
+++ cuatro.js
@@ -1,51 +1,31 @@
 function mostrar()
-{ 
+{
     var num1;
     var num2;
     var resultado;
-    
+
     num1=prompt(num1);
     num2=prompt(num2);
-    resultado=num1+num2;
+
     if(num1==num2)
     {
-        resultado=num1+num2;
+        alert(num1+num2)
     }
-    else
-    {
-    if(num1>num2)
-    {
-     dividir=(num1/num2);
-    
-    {
-       
-        else(resultado<50 && (num1<num2)
+        else
+        if(num1>num2)
         {
-            alert("el restulado es" +resultado);
-        }      
-    }
-           
+            resultado=num1-num2;
+        }
+            if(num1<num2)
+            {
+                num1=parseInt(num1);
+                num2=parseInt(num2);
+                resultado=num1+num2;
+            }
+                else(resultado>10)
+                {
+                    alert("la suma es " +resultado + "supero el 10 " );
+                }
     
-    
-
-        
-    
-        
-
-
-
-    
-
-     
-    
-
-          
-    
-        
-        
-
-        
-    
-
 
 }
