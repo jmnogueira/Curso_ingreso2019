fd9e856b89cdd100b3c0caf93f49f5cfa85a277b
jmnogueira <52511619+jmnogueira@users.noreply.github.com>
2019-07-13 21:18:59 -0300

    parcial

--- cuatro.js
+++ cuatro.js
@@ -6,26 +6,26 @@ function mostrar()
 
     num1=prompt(num1);
     num2=prompt(num2);
-    
+
     if(num1==num2)
     {
-       alert(num1+num2); 
+        alert(num1+num2)
     }
         else
         if(num1>num2)
         {
-            num1/num2
+            resultado=num1-num2;
         }
-            else(num1<num2)
+            if(num1<num2)
             {
                 num1=parseInt(num1);
                 num2=parseInt(num2);
                 resultado=num1+num2;
             }
-                if(resultado<50)
+                else(resultado>10)
                 {
-                    alert("la suma es " +resultado + "es menor a 50 ")
+                    alert("la suma es " +resultado + "supero el 10 " );
                 }
-
+    
 
 }
